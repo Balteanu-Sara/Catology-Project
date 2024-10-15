fb1c9fb1042ff923bfce588e89e4acb7cb59e0f2
7h35up3rPr0 <skipperprivate@yahoo.com>
2024-10-15 14:00:34 +0300

    p5

--- Catalogy/scripts/data_visualization.py
+++ Catalogy/scripts/data_visualization.py
@@ -5,6 +5,18 @@ import re
 
 df = pd.read_excel('../Data/cats_data1.xlsx', sheet_name='Data')
 code_df = pd.read_excel('../Data/cats_data1.xlsx', sheet_name='Code')
+df["Sexe"] = df["Sexe"].map({"F": 0, "M": 1})
+df["Age"] = df["Age"].map({"Moinsde1": 0.5, "1a2": 1.5, "2a10": 6, "Plusde10": 12.5})
+df["Race"] = df["Race"].map({"BEN": 1, "SBI": 2, "BRI": 3, "CHA": 4, "EUR": 5, "MCO": 6, "PER": 7, "RAG": 8, "SAV": 9,
+                "SPH": 10, "ORI": 11, "TUV": 12, "Autre": 0, "NSP": -1, "NR": -2})
+df["Nombre"] = df["Nombre"].map(lambda n: int(n) if n != "Plusde5" else 6)
+df["Logement"] = df["Logement"].map({"ASB": 1, "AAB": 2, "ML": 3, "MI": 4})
+df["Zone"] = df["Zone"].map({"U": 1, "PU": 2, "R": 3})
+df["Ext"] = df["Ext"].map({0: 0, 1: 0.5, 2: 3, 3: 14.5, 4: 24})
+df["Obs"] = df["Obs"].map({0: 0, 1: 0.5, 2: 3, 3: 6.5})
+df["Abondance"] = df["Abondance"].map(lambda a: int(a) if a != "NSP" else 0)
+df["PredOiseau"] = df["PredOiseau"].map({0: 0, 1: 3, 2: 7.5, 3: 24, 4: 52})
+df["PredMamm"] = df["PredMamm"].map({0: 0, 1: 3, 2: 7.5, 3: 24, 4: 52})
 
 mapping_dict = {}
 for attr in code_df['Variable'].unique():
@@ -32,7 +44,7 @@ for attr in code_df['Variable'].unique():
 
 for col in df.columns:
     if col not in ['Row.names', 'Horodateur', 'Plus']:
-        plt.figure(figsize=(10, 8))
+        plt.figure(figsize=(8, 7))
 
         # Check if the column has a mapping
         if col in mapping_dict:
